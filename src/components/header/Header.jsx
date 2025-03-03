a8a6c3cc5d3b2a86cb6031a7008edc999164d75f
andkstrr <andikasatrionurcahyo@gmail.com>
2025-03-03 11:56:02 +0700

    update detail page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { GrInstagram } from "react-icons/gr";
+import { FaFacebook, FaYoutube, FaTiktok } from 'react-icons/fa'
+
 import './header.css'
 
 const Header = () => {
   return (
     <>
       <header>
-        
+        <div className="header-container">
+          <div className="media-icons">
+            <RxHamburgerMenu size={30} color="#fff" />
+            <FaFacebook color="#fff" />
+            <FaYoutube color="#fff" size={19} />
+            <GrInstagram color="#fff" size={14} />
+            <FaTiktok color="#fff" size={15} />
+          </div>
+        </div>
       </header>
     </>
   )
